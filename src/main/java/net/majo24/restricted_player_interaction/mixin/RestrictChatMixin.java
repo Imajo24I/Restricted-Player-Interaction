b24a646da18b6b5b5b77b0a97366864d1c04f393
Martin <martin.j.schilling@gmx.de>
2024-07-16 15:43:35 +0200

    fix the disconnect message not being hidden for non permissioned

--- src/main/java/net/majo24/restricted_player_interaction/mixin/RestrictChatMixin.java
+++ src/main/java/net/majo24/restricted_player_interaction/mixin/RestrictChatMixin.java
@@ -3,8 +3,11 @@ package net.majo24.restricted_player_interaction.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +22,8 @@ public abstract class RestrictChatMixin {
 
     @Shadow
     public ServerPlayerEntity player;
+
+    @Shadow @Final private MinecraftServer server;
 
     @WrapOperation(
             method = "onChatMessage",
@@ -46,5 +51,26 @@ public abstract class RestrictChatMixin {
             return original.call(instance, runnable);
         }
         return null;
+    }
+
+    @WrapOperation(
+            method = "onDisconnected",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"
+            )
+    )
+    private void onlyBroadcastToPermissioned(PlayerManager instance, Text message, boolean overlay, Operation<Void> original) {
+        if (configManager.restrictJoinLeaveMessages()) {
+            this.server.sendMessage(message);
+
+            for (ServerPlayerEntity player : instance.getPlayerList()) {
+                if (playerHasPermission(player)) {
+                    player.sendMessageToClient(message, overlay);
+                }
+            }
+        } else {
+            original.call(instance, message, overlay);
+        }
     }
 }
