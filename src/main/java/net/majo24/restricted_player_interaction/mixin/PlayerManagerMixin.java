29fe9bd36b91f4215fd151d9a06a014c35636af7
Martin <martin.j.schilling@gmx.de>
2024-07-16 14:50:03 +0200

    cleanup

--- src/main/java/net/majo24/restricted_player_interaction/mixin/PlayerManagerMixin.java
+++ src/main/java/net/majo24/restricted_player_interaction/mixin/PlayerManagerMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
